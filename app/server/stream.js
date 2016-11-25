498987effb6176818c30483f473f22fb03bb083a
Thibault AUVRAY <tauvray@student.42.fr>
2016-11-25 12:28:20 +0100

    Save stream

--- app/server/stream.js
+++ app/server/stream.js
@@ -228,23 +228,19 @@ var streamMovie = function (data, query, range_string, res, isdownload, magnet)
                                 .on("end", function (data)
                                 {
                                     console.log("FINISH");
-                                    console.log("FINISH");
-                                    console.log("FINISH");
-                                    console.log("FINISH");
-                                    console.log("FINISH");
                                     console.log(info.path);
                                     console.log(data.name);
                                     // fs.createReadStream(converted_path).pipe(fs.createWriteStream(converted_path + '.finished.mp4'));
-                                    var newMagnet = Magnet(
-                                        {
-                                            url: magnet,
-                                            path: info.path,
-                                            name: info.file
-                                        });
-                                    newMagnet.save(function (err)
-                                    {
-                                        console.log(err);
-                                    });
+                                    // var newMagnet = Magnet(
+                                    //     {
+                                    //         url: magnet,
+                                    //         path: info.path,
+                                    //         name: info.file
+                                    //     });
+                                    // newMagnet.save(function (err)
+                                    // {
+                                    //     console.log(err);
+                                    // });
                                 })
                                 .on("error", function (err, stdout, stderr)
                                 {
@@ -340,25 +336,25 @@ var streamMovie = function (data, query, range_string, res, isdownload, magnet)
                 {
                     console.log("INFO FILE + ");
                     console.log(info);
-                    info.size = fs.statSync(info.path).size;
-                    info.rangeRequest = false;
-                    info.start = 0;
-                    info.end = info.size - 1;
-                    if (range_string && (range = range_string.match(/bytes=(.+)-(.+)?/)) !== null)
-                    {
-                        info.start = isNumber(range[1]) && range[1] >= 0 && range[1] < info.end ? range[1] - 0 : info.start;
-                        info.end = isNumber(range[2]) && range[2] > info.start && range[2] <= info.end ? range[2] - 0 : info.end;
-                        info.rangeRequest = true;
-                        console.log("Request partiel");
-                    } else if (req.query.start || req.query.end)
-                    {
-                        console.log("here");
-                        // This is a range request, but doesn't get range headers. So there.
-                        info.start = isNumber(req.query.start) && req.query.start >= 0 && req.query.start < info.end ? req.query.start - 0 : info.start;
-                        info.end = isNumber(req.query.end) && req.query.end > info.start && req.query.end <= info.end ? req.query.end - 0 : info.end;
-                    }
-                    info.length = info.end - info.start + 1;
-                    setHeaderInfo(data, info, res);
+                    // info.size = fs.statSync(info.path).size;
+                    // info.rangeRequest = false;
+                    // info.start = 0;
+                    // info.end = info.size - 1;
+                    // if (range_string && (range = range_string.match(/bytes=(.+)-(.+)?/)) !== null)
+                    // {
+                    //     info.start = isNumber(range[1]) && range[1] >= 0 && range[1] < info.end ? range[1] - 0 : info.start;
+                    //     info.end = isNumber(range[2]) && range[2] > info.start && range[2] <= info.end ? range[2] - 0 : info.end;
+                    //     info.rangeRequest = true;
+                    //     console.log("Request partiel");
+                    // } else if (req.query.start || req.query.end)
+                    // {
+                    //     console.log("here");
+                    //     // This is a range request, but doesn't get range headers. So there.
+                    //     info.start = isNumber(req.query.start) && req.query.start >= 0 && req.query.start < info.end ? req.query.start - 0 : info.start;
+                    //     info.end = isNumber(req.query.end) && req.query.end > info.start && req.query.end <= info.end ? req.query.end - 0 : info.end;
+                    // }
+                    // info.length = info.end - info.start + 1;
+                    setHeaderInfo(data, info, res, range_string);
                     try
                     {
                         console.log(info.path);
@@ -459,10 +455,62 @@ var streamMovie = function (data, query, range_string, res, isdownload, magnet)
 //                 //     .pipe(stream, {end:true});
 
 }
+
+var setHeaderInfo = function (data, info, res)
+{
+    var code = 200;
+    var header;
+
+    // 'Connection':'close',
+    // 'Cache-Control':'private',
+    // 'Transfer-Encoding':'chunked'
+
+    if (settings.forceDownload)
+    {
+        header = {
+            Expires: 0,
+            "Cache-Control": "must-revalidate, post-check=0, pre-check=0",
+            //"Cache-Control": "private",
+            "Content-Type": info.mime,
+            "Content-Disposition": "attachment; filename=" + info.file + ";"
+        };
+    } else
+    {
+        header = {
+            "Cache-Control": "public; max-age=" + settings.maxAge,
+            Connection: "keep-alive",
+            "Content-Type": info.mime,
+            "Content-Disposition": "inline; filename=" + info.file + ";",
+            "Accept-Ranges": "bytes"
+        };
+
+        if (info.rangeRequest)
+        {
+            // Partial http response
+            code = 206;
+            header.Status = "206 Partial Content";
+            header["Content-Range"] = "bytes " + info.start + "-" + info.end + "/" + info.size;
+        }
+    }
+
+    header.Pragma = "public";
+    header["Last-Modified"] = info.modified.toUTCString();
+    header["Content-Transfer-Encoding"] = "binary";
+    header["Content-Length"] = info.length;
+    if (settings.cors)
+    {
+        header["Access-Control-Allow-Origin"] = "*";
+        header["Access-Control-Allow-Headers"] = "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept";
+    }
+    header.Server = settings.server;
+
+    res.writeHead(code, header);
+}
+
 exports.torrent = function (req, res, next)
 {
     var io = req.io;
-    var range_string = req.headers.range;
+    var range_string = req.headers['range'];
     // MP4
     // var magnet = "magnet:?xt=urn:btih:93293ef1db6d2ccbe298f5605777476e75ad472e&dn=Rick+And+Morty+S01E08+HDTV+x264-MiNDTHEGAP+%5Beztv%5D&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Fzer0day.ch%3A1337&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969";
     // AVI
@@ -776,53 +824,3 @@ var isNumber = function (n)
     return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-var setHeaderInfo = function (data, info, res)
-{
-    var code = 200;
-    var header;
-
-    // 'Connection':'close',
-    // 'Cache-Control':'private',
-    // 'Transfer-Encoding':'chunked'
-
-    if (settings.forceDownload)
-    {
-        header = {
-            Expires: 0,
-            "Cache-Control": "must-revalidate, post-check=0, pre-check=0",
-            //"Cache-Control": "private",
-            "Content-Type": info.mime,
-            "Content-Disposition": "attachment; filename=" + info.file + ";"
-        };
-    } else
-    {
-        header = {
-            "Cache-Control": "public; max-age=" + settings.maxAge,
-            Connection: "keep-alive",
-            "Content-Type": info.mime,
-            "Content-Disposition": "inline; filename=" + info.file + ";",
-            "Accept-Ranges": "bytes"
-        };
-
-        if (info.rangeRequest)
-        {
-            // Partial http response
-            code = 206;
-            header.Status = "206 Partial Content";
-            header["Content-Range"] = "bytes " + info.start + "-" + info.end + "/" + info.size;
-        }
-    }
-
-    header.Pragma = "public";
-    header["Last-Modified"] = info.modified.toUTCString();
-    header["Content-Transfer-Encoding"] = "binary";
-    header["Content-Length"] = info.length;
-    if (settings.cors)
-    {
-        header["Access-Control-Allow-Origin"] = "*";
-        header["Access-Control-Allow-Headers"] = "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept";
-    }
-    header.Server = settings.server;
-
-    res.writeHead(code, header);
-}
