039ebf7090866d806f4991ef2b8fc3bdc3d2a483
Seif HABBACHI <shabbach@e2r3p8.42.fr>
2016-12-08 14:16:59 +0100

    server/library.js

--- app/server/library.js
+++ app/server/library.js
@@ -1,13 +1,11 @@
 var mongoose = require('../mongoose'),
 	User = require('../user_schema'),
-	Top100 = require('../Top100_schema'),
+	Movie = require('../movie_schema'),
 	_ = require('lodash');
 
 var library = function(req, res, next) {
 	User.findOne({ username : req.session.username }, function(err, user) {
 		if (!err && user) {
-			 //var elem = user.history;
-			 // console.log(elem);
 			res.render('library', {
 				isApp : true,
 				title : 'Hypertube - Library',
