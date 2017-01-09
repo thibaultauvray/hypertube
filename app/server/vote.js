448011dde3a10ac96a5481aa53d25cc9959aa284
Seif HABBACHI <shabbach@e2r3p8.42.fr>
2017-01-09 15:21:16 +0100

    votes fix

--- app/server/vote.js
+++ app/server/vote.js
@@ -2,7 +2,7 @@ var mongoose = require('../mongoose'),
     User = require('../user_schema'),
     Movie = require('../movie_schema'),
     async = require("async");
-
+/*
 var vote = function (req, res) {
     User.findOne({ username : req.session.username }, function(err, user) {
         if (!err && user){
@@ -11,6 +11,7 @@ var vote = function (req, res) {
                 var dislike = movie.dislike;
                 var like = movie.like;
                 var voted = false;
+
                 async.each(tmpVotes, function (vote, err) {
                     if (vote.torrentId == req.body.torrentId) {
                         if (vote.which != req.body.vote){
@@ -50,10 +51,11 @@ var vote = function (req, res) {
                         Movie.update({'torrent.id': req.body.torrentId}, {$set: {dislike: dislike, like : like}}, function (err, doc) {
                             if (err) console.log(err);
                         })
-                        User.update({username: req.session.username}, {$addToSet: {votes: {'which':req.body.vote, 'torrentId':req.body.torrentId}}}, function (err, u) {
+                        User.update({'username': req.session.username}, {$addToSet: {'votes': {'which':req.body.vote, 'torrentId':req.body.torrentId}}}, function (err, u) {
                             if (err) console.log(err);
                         });
                     }
+                    voted = false;
                     res.json({dislikes: dislike, likes: like});
                 })
 
@@ -64,6 +66,88 @@ var vote = function (req, res) {
         }
     })
 }
+*/
 
+var vote = function (req, res) {
+    User.findOne({ username : req.session.username }, function(err, user) {
+        if (!err && user) {
+            Movie.findOne({'torrent.id': req.body.torrentId}, function (error, movie) {
+                if (!error && movie) {
+                    console.log(req.body.vote, " movie.like " + movie.like , " movie.dislike " + movie.dislike);
+                    var voted = false;
+                    var tmpVotes = user.votes;
+                    var update = false;
+                    tmpVotes.forEach(function (e) {
+                        if (e.torrentId == req.body.torrentId){
+                            if (e.which){
+                                if (req.body.vote == 'down' && e.which == 'up'){
+                                    var dislike = movie.dislike + 1;
+                                    if (movie.like > 0)
+                                        var like = movie.like - 1;
+                                    else
+                                        var like = movie.like;
+                                    update = true;
+                                } else if (req.body.vote == 'up' && e.which == 'down'){
+                                    var like = movie.like + 1;
+                                    if (movie.dislike > 0)
+                                        var dislike = movie.dislike - 1;
+                                    else
+                                        var dislike = movie.dislike;
+                                    update = true;
+                                }
+                                e.which = req.body.vote;
+                                voted = true;
+                                if (update) {
+                                    Movie.update({'torrent.id': req.body.torrentId}, {
+                                        $set: {
+                                            dislike: dislike,
+                                            like: like
+                                        }
+                                    }, function (err, doc) {
+                                        if (err) console.log(err);
+                                    });
+                                    User.update({username: req.session.username}, {$set: {votes: tmpVotes}}, function (err, u) {
+                                        if (err) console.log(err);
+                                    });
+                                    res.json({dislikes: dislike, likes: like});
+                                }
+                            }else {
+                                console.log("pas encore vote");
+                            }
+                        }
+                    });
+                    if (!voted){
+                        if (req.body.vote == 'down') {
+                            var dislike = movie.dislike + 1;
+                            var like = movie.like;
+                        } else if (req.body.vote == 'up') {
+                            var like = movie.dislike + 1;
+                            var dislike = movie.dislike;
+                        }
+                        Movie.update({'torrent.id': req.body.torrentId}, {
+                            $set: {
+                                dislike: dislike,
+                                like: like
+                            }
+                        }, function (err, doc) {
+                            if (err) console.log(err);
+                        });
+                        User.update({username: req.session.username}, {
+                            $addToSet: {
+                                'votes': {
+                                    'which': req.body.vote,
+                                    'torrentId': req.body.torrentId
+                                }
+                            }
+                        }, function (err, u) {
+                            if (err) console.log(err);
+                        });
+                        res.json({dislikes: dislike, likes: like});
+                    }
+                }
+            })
+        }
+    })
+}
 
 module.exports = vote;
